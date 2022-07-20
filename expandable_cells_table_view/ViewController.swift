7cfe34e4c73ba35ebb3c2fe66bfcb7629493d91d
Kyle Essenmacher <kessen@umich.edu>
2022-07-20 10:01:03 -0400

    trying to figure out how to put API calls in the table next

---
+++ expandable_cells_table_view/ViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  expandable_cells_table_view
+//
+//  Created by Kyle Essenmacher on 7/19/22. using this video https://www.youtube.com/watch?v=samoIVHg6yQ&t=528s
+//
+
+import UIKit
+
+class Section {
+    let title: String
+    let options: [String]
+    var isOpened: Bool = false
+    
+    init(title: String,
+         options: [String],
+         isOpened: Bool = false
+    ) {
+        self.title = title
+        self.options = options
+        self.isOpened = isOpened
+    }
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    private var sections = [Section]()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Setup Models
+        sections = [
+            Section(title: "Section 1", options: [1, 2, 3].compactMap({ return "Cell \($0)" })),
+            Section(title: "Section 2", options: [1, 2, 3].compactMap({ return "Cell \($0)" })),
+            Section(title: "Section 3", options: [1, 2, 3].compactMap({ return "Cell \($0)" })),
+            Section(title: "Section 4", options: [1, 2, 3].compactMap({ return "Cell \($0)" })),        ]
+        
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.frame = view.bounds
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = sections[section]
+        
+        if section.isOpened {
+            return section.options.count + 1
+        }
+        else{
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "cell",
+            for: indexPath
+        )
+        if indexPath.row == 0 {
+            cell.textLabel?.text = sections[indexPath.section].title
+        }
+        else {
+            
+            cell.textLabel?.text = sections[indexPath.section].options[indexPath.row - 1]
+            
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        sections[indexPath.section].isOpened = !sections[indexPath.section].isOpened
+        tableView.reloadSections([indexPath.section], with: .none)
+    }
+    
+}
+
